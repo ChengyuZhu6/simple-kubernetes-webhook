d89946526963a772c02ff064dc9defc00d20074b
ChengyuZhu6 <chengyu.zhu@intel.com>
2024-04-07 10:06:21 +0800

    support to create k8s client
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>

---
+++ pkg/utils/utils.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2024 Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package utils
+
+import (
+	"github.com/pkg/errors"
+	"k8s.io/client-go/kubernetes"
+	kubernetesConfig "sigs.k8s.io/controller-runtime/pkg/client/config"
+)
+
+var k8sClient kubernetes.Interface
+
+func NewK8SClient() (kubernetes.Interface, error) {
+	if k8sClient != nil {
+		return k8sClient, nil
+	}
+	kubeConfig, err := kubernetesConfig.GetConfig()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get kubernetes config")
+	}
+
+	k8sClient, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create kubernetes client")
+	}
+	return k8sClient, nil
+}
