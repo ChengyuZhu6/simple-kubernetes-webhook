f53698b704702d6827c0ccdebb431e94b519e687
ChengyuZhu6 <chengyu.zhu@intel.com>
2024-04-17 11:08:21 +0800

    add cdh rpc functions
    Signed-off-by: ChengyuZhu6 <chengyu.zhu@intel.com>

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -4,16 +4,19 @@
 package api
 
 import (
+	"context"
 	"fmt"
 	"net"
+	"strings"
 
 	"github.com/containerd/ttrpc"
 	cdh "github.com/slackhq/simple-kubernetes-webhook/pkg/api/cdh"
 )
 
 const (
-	defaultMinTimeout = 60
-	CDHSocket         = "/run/confidential-containers/cdh.sock"
+	defaultMinTimeout  = 60
+	CDHSocket          = "/run/confidential-containers/cdh.sock"
+	sealedSecretPrefix = "sealed."
 )
 
 type cdhClient struct {
@@ -35,4 +38,30 @@ func CreateCDHClient() (*cdhClient, error) {
 		sealedSecretClient: client,
 	}
 	return c, nil
+}
+
+func (c *cdhClient) Close() error {
+	return c.conn.Close()
+}
+
+func (c *cdhClient) UnsealSecret(ctx context.Context, secret string) (string, error) {
+	input := cdh.UnsealSecretInput{Secret: []byte(secret)}
+	output, err := c.sealedSecretClient.UnsealSecret(ctx, &input)
+	if err != nil {
+		return "", fmt.Errorf("failed to unseal secret: %w", err)
+	}
+
+	return string(output.GetPlaintext()[:]), nil
+}
+
+func (c *cdhClient) UnsealEnv(ctx context.Context, env string) (string, error) {
+	unsealedValue, err := c.UnsealSecret(ctx, env)
+	if err != nil {
+		return "", fmt.Errorf("failed to unseal secret from env: %w", err)
+	}
+	return unsealedValue, nil
+}
+
+func HasSealedSecretsPrefix(value string) bool {
+	return strings.HasPrefix(value, sealedSecretPrefix)
 }
